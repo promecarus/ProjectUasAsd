84512e8143f4b7492a0b7c6d50078d6da2c39f43
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-07-13 21:58:20 +0700

    awal

---
+++ nomor1.cpp
@@ -0,0 +1,12 @@
+// Buatlah suatu program antrean pasien pada suatu klinik ABC. Program dapat menerima inputan nama dan no ID pasien, kemudian program akan menentukan nomor urut pasien dan waktu kedatangan pasien. Silahkan tambahkan asumsi seperti perkiraan waktu periksa yang digunakan per pasien. Buatlah tampilan aplikasi semenarik mungkin
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    
+	system("pause");
+	return 0;
+}
