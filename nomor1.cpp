6c248aaf4ff164e1fae034701839af7c9e748d52
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2022-07-19 23:51:19 +0700

    progres

--- nomor1.cpp
+++ nomor1.cpp
@@ -4,8 +4,11 @@
 // seperti perkiraan waktu periksa yang digunakan per pasien. Buatlah tampilan
 // aplikasi semenarik mungkin
 
+#include <ctime>
 #include <iomanip>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -14,7 +17,7 @@ struct Pasien {
     string nama;
     int id;
     int nomorUrut;
-    int waktuKedatangan;
+    string waktuKedatangan;
 
     Pasien *next;
 };
@@ -24,6 +27,46 @@ int maksimal = 5;
 
 // pointer
 Pasien *head, *tail, *cur, *del, *newNode;
+
+// waktu kedatangan
+int nomorUrutCounter = 0;
+
+// border menu
+void tampilanMenu() {
+    int lebarTabel = 34;
+    string menu[] = {"[0] Keluar", "[1] Tambah Antrean", "[2] Panggil Pasien",
+                     "[3] Lihat List Antrean", "[4] Bersihkan List Antrean"};
+    cout << "\n"
+         << setfill('-') << setw(lebarTabel) << left << "+-"
+         << "+\n";
+    cout << setfill(' ') << "| " << setw(lebarTabel - 2) << left
+         << "Menu Kontrol Antrean Klinik ABC"
+         << "|";
+    cout << "\n"
+         << setfill('-') << setw(lebarTabel) << left << "+-"
+         << "+\n";
+    for (int i = 0; i < sizeof(menu) / sizeof(string); i++) {
+        cout << setfill(' ') << "| " << setw(lebarTabel - 2) << left << menu[i]
+             << "|\n";
+    }
+    cout << setfill('-') << setw(lebarTabel) << left << "+-"
+         << "+\n";
+    cout << "Input Anda: ";
+}
+
+bool konfirmasiKeluar() {
+    system("cls");
+    bool hasil;
+    string input;
+    cout << "Apakah anda yakin? [y/n]: ";
+    cin >> input;
+    if (tolower(input[0]) == 'y') {
+        system("cls");
+        cout << "Terima kasih telah menggunakan aplikasi ini.";
+        hasil = true;
+    }
+    return !hasil;
+}
 
 // count
 int count() {
@@ -38,9 +81,6 @@ int count() {
 
     return counter;
 }
-
-// waktu kedatangan
-int waktuKedatanganCounter = 1;
 
 // is full
 bool isFull() { return (count() == maksimal); }
@@ -57,15 +97,24 @@ void enqueue() {
         newNode = new Pasien();
 
         // penginputan field nama dan id oleh user
+        cin.ignore();
         cout << "Nama : ";
-        cin >> newNode->nama;
+        // cin >> newNode->nama;
+        // cin >> newNode->nama;
+        getline(cin, newNode->nama);
+
         cout << "ID   : ";
         cin >> newNode->id;
 
         // penentuan field nomor urut dan waktu kedatangan oleh program
-        newNode->nomorUrut = count() + 1;
-        newNode->waktuKedatangan = waktuKedatanganCounter;
-        waktuKedatanganCounter++;
+        nomorUrutCounter++;
+        newNode->nomorUrut = nomorUrutCounter;
+
+        auto t = time(nullptr);
+        auto tm = *localtime(&t);
+        ostringstream oss;
+        oss << put_time(&tm, "%d-%m-%Y %H:%M:%S");
+        newNode->waktuKedatangan = oss.str();
 
         // pengnullan field next
         newNode->next = NULL;
@@ -95,29 +144,61 @@ void dequeue() {
 
 // display
 void display() {
+    int ruasTabelDisplay[] = {6, 28, 19, 13, 22};
     if (isEmpty()) {
         cout << "Antrean kosong." << endl;
     } else {
         cur = head;
         int nomorBaris = 1;
-
-        while (nomorBaris <= maksimal) {
+        string kosong = "";
+        cout << setfill('-') << setw(ruasTabelDisplay[0]) << left << "+-"
+             << setw(ruasTabelDisplay[1]) << left << "+-"
+             << setw(ruasTabelDisplay[2]) << left << "+-"
+             << setw(ruasTabelDisplay[3]) << left << "+-"
+             << setw(ruasTabelDisplay[4]) << left << "+-"
+             << "+\n";
+        cout << setfill(' ') << "| " << left << setw(ruasTabelDisplay[0] - 2)
+             << "No."
+             << "| " << left << setw(ruasTabelDisplay[1] - 2) << "Nama"
+             << "| " << left << setw(ruasTabelDisplay[2] - 2) << "Nomor ID"
+             << "| " << left << setw(ruasTabelDisplay[3] - 2) << "Nomor Urut"
+             << "| " << left << setw(ruasTabelDisplay[4] - 2)
+             << "Waktu Kedatangan"
+             << "|\n";
+        do {
+            cout << setfill('-') << setw(ruasTabelDisplay[0]) << left << "+-"
+                 << setw(ruasTabelDisplay[1]) << left << "+-"
+                 << setw(ruasTabelDisplay[2]) << left << "+-"
+                 << setw(ruasTabelDisplay[3]) << left << "+-"
+                 << setw(ruasTabelDisplay[4]) << left << "+-"
+                 << "+\n";
             if (cur != NULL) {
-                cout << nomorBaris << " " << cur->id << " " << cur->nama << " "
-                     << cur->nomorUrut << " " << cur->waktuKedatangan << endl;
+                cout << setfill(' ') << "| " << left
+                     << setw(ruasTabelDisplay[0] - 2) << nomorBaris << "| "
+                     << left << setw(ruasTabelDisplay[1] - 2) << cur->nama
+                     << "| " << left << setw(ruasTabelDisplay[2] - 2) << cur->id
+                     << "| " << left << setw(ruasTabelDisplay[3] - 2)
+                     << cur->nomorUrut << "| " << left
+                     << setw(ruasTabelDisplay[4] - 2) << cur->waktuKedatangan
+                     << "|\n";
                 cur = cur->next;
             } else {
-                cout << nomorBaris << " "
-                     << "cur->id"
-                     << " "
-                     << "cur->nama"
-                     << " "
-                     << "cur->nomorUrut"
-                     << " "
-                     << "cur->waktuKedatangan" << endl;
+                cout << setfill(' ') << "| " << left
+                     << setw(ruasTabelDisplay[0] - 2) << nomorBaris << "| "
+                     << left << setw(ruasTabelDisplay[1] - 2) << kosong << "| "
+                     << left << setw(ruasTabelDisplay[2] - 2) << kosong << "| "
+                     << left << setw(ruasTabelDisplay[3] - 2) << kosong << "| "
+                     << left << setw(ruasTabelDisplay[4] - 2) << kosong
+                     << "|\n";
             }
             nomorBaris++;
-        }
+        } while (nomorBaris <= maksimal);
+        cout << setfill('-') << setw(ruasTabelDisplay[0]) << left << "+-"
+             << setw(ruasTabelDisplay[1]) << left << "+-"
+             << setw(ruasTabelDisplay[2]) << left << "+-"
+             << setw(ruasTabelDisplay[3]) << left << "+-"
+             << setw(ruasTabelDisplay[4]) << left << "+-"
+             << "+\n";
     }
 }
 
@@ -142,50 +223,24 @@ void clear() {
 // operasi menu
 void menu() {
     bool isStay = true;
-    // while (isStay) {
-    //     system("cls");
-    //     int inputOpsi;
-    //     display();
-    //     cout << "Menu: ";
-    //     cin >> inputOpsi;
-
-    //     switch (inputOpsi) {
-    //         case 1:
-    //             enqueue();
-    //             break;
-    //         case 2:
-    //             display();
-    //             break;
-    //         case 3:
-    //             dequeue();
-    //             break;
-    //         case 4:
-    //             clear();
-    //             break;
-    //         default:
-    //             cout << "Opsi yang anda masukkan tidak valid." << endl;
-    //             break;
-    //     }
-    // }
-    do {
-        system("cls");
+    while (isStay) {
+        // system("cls");
         int inputOpsi;
-        display();
-        cout << "Menu: ";
+        tampilanMenu();
         cin >> inputOpsi;
 
         switch (inputOpsi) {
             case 0:
-                isStay = false;
+                isStay = konfirmasiKeluar();
                 break;
             case 1:
                 enqueue();
                 break;
             case 2:
-                display();
+                dequeue();
                 break;
             case 3:
-                dequeue();
+                display();
                 break;
             case 4:
                 clear();
@@ -194,10 +249,11 @@ void menu() {
                 cout << "Opsi yang anda masukkan tidak valid." << endl;
                 break;
         }
-    } while (isStay);
+    }
 }
 
 int main() {
     menu();
+    display();
     return 0;
 }
